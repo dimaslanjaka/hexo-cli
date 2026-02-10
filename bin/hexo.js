4a8ba801cd3b666ecdba621c6a59168489804fa1
dimaslanjaka <dimaslanjaka@gmail.com>
2026-02-10 22:53:09 +0700

    fix(bin): streamline hexo.js to use direct ESM import for hexoCli

--- bin/hexo.js
+++ bin/hexo.js
@@ -1,9 +1,6 @@
-#!/usr/bin/env node
-
 // eslint-disable-next-line strict
 'use strict';
 
-// require('../dist/hexo.cjs')();
-import('../dist/hexo.js').then(({ default: hexo }) => {
-  hexo();
-});
+import hexoCli from '../dist/hexo.js';
+
+hexoCli();
