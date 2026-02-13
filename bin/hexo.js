48994137f5a6ca824aab0ce6a146069dfe1d0f54
dimaslanjaka <dimaslanjaka@gmail.com>
2026-02-13 12:10:09 +0700

    fix(hexo-cli): refactor hexo loading logic to support dynamic imports for ESM and CJS compatibility

--- bin/hexo.js
+++ bin/hexo.js
@@ -1,6 +1,11 @@
 // eslint-disable-next-line strict
 'use strict';
 
-import hexoCli from '../dist/hexo.js';
-
-hexoCli();
+if (typeof require == 'undefined') {
+  import('../dist/hexo.js').then((lib) => {
+    (lib.default || lib)();
+  });
+} else {
+  const lib = require('../dist/hexo.cjs');
+  (lib.default || lib)();
+}
